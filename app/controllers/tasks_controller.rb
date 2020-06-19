8430ac43eb63318844371bf0a1ceda82d495d168
Armando Teran <ajteranv@gmail.com>
2020-06-19 19:01:41 -0300

    added update and destroy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,9 +17,18 @@ class TasksController < ApplicationController
     redirect_to tasks_path
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to tasks_path
+  end
+
   private
 
   def task_params
-    params.require(:task).permit(:name, :details)
+    params.require(:task).permit(:name, :details, :completed)
   end
 end
